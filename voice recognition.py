5ee53a76d9b1eddc52c62a67566dd0be04d534e0
ayushmaan-mishra203 <142373193+ayushmaan-mishra203@users.noreply.github.com>
2024-07-31 20:19:13 +0530

    firstcommit

---
+++ voice recognition.py
@@ -0,0 +1,34 @@
+import speech_recognition as sr
+from pyfirmata import Arduino, SERVO , util
+from time import sleep
+
+r=sr.Recognizer()
+mic = sr.Microphone(device_index=0)
+
+port = 'COM3'
+pin = 10
+board = Arduino(port)
+
+board.digital[pin].mode = SERVO 
+
+def rotate(pin, angle):
+    board.digital[pin].write(angle)
+    sleep(0.015)
+
+
+with mic as source:
+    r.adjust_for_ambient_noise(source)
+
+    while True:
+        audio = r.listen(source)
+        try:
+            if r.recognize_google(audio) =='switch on':
+                print("switch is on")
+                for i in range(0,90):
+                    rotate(pin,i)
+
+            else:
+                print("something")   
+
+        except:
+            print("no audio")
